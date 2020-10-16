fb5d5245ed93ee4af6540f7359a0f43ef5cb8612
Rachman Aldiansyah <rachmanaldiansyah03@gmail.com>
2020-10-16 14:46:38 +0700

    Upload Project

---
+++ src/main/java/com/rsoftware/if1/latihan1/IF110119038Latihan1.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.rsoftware.if1.latihan1;
+
+/**
+ *
+ * @author
+ * NAMA  : Rachman Aldiansyah
+ * KELAS : IF1
+ * NIM   : 10119038
+ * Desrkipsi Program : Program ini berisi untuk menampilkan value ke layar
+ * 
+ */
+
+public class IF110119038Latihan1 {
+    public static void main(String[] args) {
+        System.out.println("BIODATA ROCKSTAR");
+        System.out.println("================================");
+        System.out.println("NAMA\t\t: Rachman Aldiansyah");
+        System.out.println("ALAMAT\t\t: Perum.Puteraco Blok C7 No.4");
+        System.out.println("TEMPAT LAHIR\t: Bandung");
+        System.out.println("TANGGAL LAHIR\t: 15 Agustus 2001");
+        System.out.println("================================");
+        System.out.println("HOBI\t\t: Musik & Games");
+        System.out.println("MAKES\t\t: Baso & Cumi");
+        System.out.println("MIKES\t\t: Cendol");
+    }
+}
